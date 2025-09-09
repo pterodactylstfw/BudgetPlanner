48c9c72f4f3f98946ec2682de337f97c47cea3e9
Raul-Nicolae Constantin <constantin.raul10@gmail.com>
2025-09-09 19:22:34 +0300

    initial commit

---
+++ BudgetPlanner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetPlanner;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BudgetPlanner/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BudgetPlanner;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
